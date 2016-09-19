6bdcc1eefe2479b121ea7d1bc8fdc99a9182c759
Leo <Leo@bouloc.eu>
2016-09-19 02:02:41 +0200

    added left/right compare

--- wave_compare.py
+++ wave_compare.py
@@ -38,27 +38,36 @@ def compare_wavs(sound_1, sound_2, print_additional_comment='', plot=False):
     if plot:
         plot_files(sound_1, sound_2, print_additional_comment)
 
-def plot_files(sound_1, sound_2, print_additional_comment=''):
+def plot_files(sound_1, sound_2, print_additional_comment='', side='left'):    
+    
+    sides = {'left':0, 'right':1}    
+    
     plt.figure()
     plt.title(print_additional_comment)
     ax = plt.subplot(211)
-    ax.step(range(sound_1[1].shape[0]), sound_1[1])
-    ax.step(range(sound_2[1].shape[0]), sound_2[1])
+    ax.step(range(sound_1[1].shape[0]), sound_1[1][:, sides[side]])
+    ax.step(range(sound_2[1].shape[0]), sound_2[1][:, sides[side]])
     ax.set_title('Full wavs')
     
     ax = plt.subplot(212)
-    ax.step(range(sound_1[1].shape[0]), sound_2[1] - sound_1[1]) 
+    ax.step(range(sound_1[1].shape[0]), sound_2[1][:, sides[side]] \
+                        - sound_1[1][:, sides[side]]) 
     ax.set_title('Difference')
 
 def normalise_by_std(sound_1, sound_2):
     return (sound_2[0], sound_2[1]*np.std(sound_1[1])/np.std(sound_2[1]))
 
 if __name__ == '__main__':
+    ## PARAMETERS
 
     # Files to load
     file_1_path = 'sound_card_test_new.wav'
     file_2_path = 'sound_card_test_real.wav'
     
+    # Show plots (only if you have matplotlib):
+    do_plot = False
+    
+    # RUN 
     # Open wave files and put in numpy array
     sound_1 = wavfile.read(file_1_path)
     sound_2 = wavfile.read(file_2_path)
@@ -67,5 +76,5 @@ if __name__ == '__main__':
     sound_2_norm = normalise_by_std(sound_1, sound_2)
     
     # Compare new file and normalised new file with the original
-    compare_wavs(sound_1, sound_2, '\n*** Orig file ***', plot=True)
-    compare_wavs(sound_1, sound_2_norm, '\n*** Normalised file ***', plot=True)
+    compare_wavs(sound_1, sound_2, '\n*** Orig file ***', plot=do_plot)
+    compare_wavs(sound_1, sound_2_norm, '\n*** Normalised file ***', plot=do_plot)
